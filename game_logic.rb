70402492297056ca5b0eaa44370cbdc515f9729e
Mara <125482894+Mara2403@users.noreply.github.com>
2024-01-22 11:52:44 +0100

    Update game_logic.rb and human_player.rb
    - Add new methods in GameLogic module
      - exact_good
      - good_guess_wrong_position_array
      - compare_codes
    - Move methods from HumanPlayer class into GameLogic module

--- game_logic.rb
+++ game_logic.rb
@@ -1,3 +1,46 @@
 # frozen_string_literal: true
 
-require_relative 'display'
+# Logic used to play the game
+module GameLogic
+  def exact_good(right_code, guess)
+    exact_clues_array = []
+    guess.each_with_index do |num, index|
+      exact_clues_array << (right_code[index] == num ? 'x' : right_code[index])
+    end
+    exact_clues_array
+    # show_clues(clues_array)
+  end
+
+  def good_guess_wrong_position_array(code, guess)
+    good_guess_array = []
+
+    guess.each_with_index do |num, index|
+      good_guess_array << (code[index] == 'x' ? 'x' : num)
+    end
+    good_guess_array
+  end
+
+  def compare_codes(right_code, guess)
+    temp_break_code = right_code.clone
+    temp_code = exact_good(temp_break_code, guess)
+    print temp_code # it shows original CODE to break array, with 'x' in place where the GUESS number is in exact good place
+
+    # now remove those exact good guessed numbers from the user guess array
+    # user guess array with x :
+    work_array = good_guess_wrong_position_array(temp_code, guess)
+
+    # the final comparing
+    final_clue_array = []
+    work_array.each_with_index do |num, index|
+      if temp_code[index] == 'x'
+        final_clue_array << 'x'
+      elsif temp_code.include?(num)
+        final_clue_array << 'o'
+      end
+      final_clue_array
+    end
+    print final_clue_array
+    sorted_array = final_clue_array.sort { |a| a == 'x' ? -1 : 1 }
+    show_clues(sorted_array)
+  end
+end
--- human_player.rb
+++ human_player.rb
@@ -15,16 +15,10 @@ class HumanPlayer
   end
 
   def play_game
-    puts player_instructions
+    # puts player_instructions
     puts "# Computer generated #{@code_to_break}" # to be removed later
     user_guess = gets.chomp.split('').map(&:to_i)
-    print user_guess
+    show_code(user_guess)
     compare_codes(@code_to_break, user_guess)
-  end
-
-  def compare_codes(right_code, guess)
-    if right_code == guess
-      puts ingame_text('user_won')
-    end
   end
 end
