389647ec2603c2fe109a38b4e2e15a79e6032348
Mara <125482894+Mara2403@users.noreply.github.com>
2024-02-06 10:44:09 +0100

    Refactor file organization:
    - Delete main.rb
    - Rename game.rb to main.rb
    - Instantiate Game class in main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 require_relative 'text_content'
-require_relative 'game'
+require_relative 'display'
+require_relative 'human_player'
+require_relative 'computer_player'
+require_relative 'game_logic'
+
+# Starts new game for the code maker or the breaker
+class Game
+  include TextContent
+  include Display
+  include GameLogic
+
+  def play
+    puts game_intro
+    choose_mode
+  end
+end
 
 Game.new.play
