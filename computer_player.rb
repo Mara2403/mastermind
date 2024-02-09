8843bb1030ee058eb1a60cb91a37c6b92e7935dd
Mara <125482894+Mara2403@users.noreply.github.com>
2024-02-09 11:41:39 +0100

    Modify multiple files
    - Rename computer.player. to computer_player.rb
    - Modify play_game method in ComputerPlayer
    - Modify TextContent module
    - Modify use_feedback method in GameLogic
    - Remove debug output
        -removed unnecessary 'puts' statements
    - Made some RuboCop changes

---
+++ computer_player.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require_relative 'display'
+require_relative 'text_content'
+require_relative 'game_logic'
+
+# Creates version of the game for a computer as the code breaker. Computer uses only the feedback of exact good numbers
+class ComputerPlayer
+  include TextContent
+  include Display
+  include GameLogic
+
+  def initialize
+    puts computer_game_instructions
+    @code_to_break = user_four_numbers
+    @possible_combinations = (1..6).to_a.repeated_permutation(4).to_a.map do |combination|
+      combination.map(&:to_i)
+    end
+    @guessed_combinations = []
+  end
+
+  def play_game
+    puts "User code to crack by the computer: #{@code_to_break}"
+    break_code = @code_to_break.split('').map(&:to_i)
+    12.times do |turn|
+      random_guess = @possible_combinations.sample
+
+      puts "Turn #{turn + 1}. Computer guess: "
+      show_code(random_guess)
+      if game_won?(break_code, random_guess)
+        puts ingame_text('computer_won')
+        user_choice = play_again?
+        choose_mode if user_choice == 'y'
+        puts ingame_text('break') if user_choice == 'n'
+        break
+      end
+      compare_codes(break_code, random_guess)
+      feedback_array = feedback_matches_array(break_code, random_guess)
+      @possible_combinations = use_feedback(@possible_combinations, random_guess, feedback_array)
+      next unless turn == 11
+
+      puts ingame_text('computer_lost')
+      user_choice = play_again?
+      choose_mode if user_choice == 'y'
+      puts ingame_text('break') if user_choice == 'n'
+    end
+  end
+end
